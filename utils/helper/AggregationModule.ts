f7f1524205a91008887419b6d2b51a8c6b55cf6e
Vishnu <vishnukant282@gmail.com>
2024-10-17 21:24:06 +0530

    commit

--- utils/helper/AggregationModule.ts
+++ utils/helper/AggregationModule.ts
@@ -7,11 +7,12 @@ import {
   Struct,
   ZkProgram,
   SelfProof,
-} from "o1js";//o1js is a typescript library for writing zk smartcontracts for MINA
+} from "o1js"; //o1js is a knn library for writing zk smartcontracts for MINA
 
-class PriceAggregationArray20 extends Struct({//inheritng struct class from o1js
-  pricesArray: Provable.Array(UInt64, 20),//defining array of size 20
-  count: UInt64,//defingn variable for incrementing
+class PriceAggregationArray20 extends Struct({
+  //inheritng struct class from o1js
+  pricesArray: Provable.Array(UInt64, 20), //defining array of size 20
+  count: UInt64, //defingn variable for incrementing
 }) {
   constructor(value: { pricesArray: UInt64[]; count: UInt64 }) {
     super(value);
